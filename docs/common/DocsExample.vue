fad3c1514893c26a341a5fab6893873e9567fd5e
chetan2112204 <chetan44rt@gmial.com>
2024-12-11 20:09:33 +0530

    resolved errors in DocsExample

--- docs/common/DocsExample.vue
+++ docs/common/DocsExample.vue
@@ -3,13 +3,15 @@
 
 
     <div class="code-toggle-button">
-      <button @click="toggleCodeVisibility">
-        <span>&lt;/&gt;</span>
-      </button>
-    </div>
+      <KIconButton  class="code-toggle"
+      :icon="isCodeVisible ? 'chevronUp' : 'codeToggle'"
+      tooltip="Toggle code visibility"
+      @click="toggleCodeVisibility"
+    />
+        </div>
 
         <!-- Toggle code  -->
-    <div v-if="isCodeVisible" class="code-container">
+    <div v-show="isCodeVisible" class="code-container">
       <slot name="code" />
     </div>
 
@@ -18,27 +20,32 @@
       <slot />
     </div>
 
-    
+
   </div>
 </template>
 
 
 <script>
+  import KIconButton from "../../lib/buttons-and-links/KIconButton.vue";
+
   export default {
     name: "DocsExample",
+    components: {
+    KIconButton
+  },
     data() {
       return {
-        isCodeVisible: false, 
+        isCodeVisible: false,
       };
     },
     methods: {
       toggleCodeVisibility() {
-        this.isCodeVisible = !this.isCodeVisible; 
+        this.isCodeVisible = !this.isCodeVisible;
       },
     },
   };
   </script>
-  
+
 
 
   <style scoped>
@@ -46,35 +53,34 @@
       margin-bottom: 24px;
       padding-top: 0;
       padding: 20px;
+
       /* border: 1px solid #ddd; */
       border-radius: 4px;
     }
-    
+
     .code-toggle-button {
       margin-bottom: 16px;
       display: flex;
       justify-content: flex-end;
     }
-    
-    button {
+
+    .code-toggle {
       padding: 8px 16px;
-      background-color: #007bff;
+      background-color: #f5d858;
       color: white;
       border: none;
       border-radius: 4px;
       cursor: pointer;
       transition: background-color 0.3s ease;
     }
-    
-    button:hover {
+
+    .code-toggle:hover {
       background-color: #0056b3;
     }
-    
-    
+
+
     .code-container {
      border: none;
-     transition: all 2s ease-in-out; /* Adjusted duration for smoother transition */
 
     }
     </style>
-    
